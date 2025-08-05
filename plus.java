3623c59cf381fcc0ee204dbc6f965f98960e2c3a
Sakshi-uc <aashisharma484@gmail.com>
2025-08-05 11:37:28 +0530

    Update plus.java

---
+++ plus.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+
+        double sum = a + b;
+
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
+}
